50504e8a79074b419c9ab794b6529513d9409ec3
Saturn172 <47@ica.org>
2018-03-03 14:54:48 +0100

    IPP - parse.php v1.3

--- IPP/parse.php
+++ IPP/parse.php
@@ -1,6 +1,28 @@
 #!/usr/bin/php5.6
 <?php
 
+$opt = getopt("a", array("help", "stats:", "loc", "comments"));
+
+if(count($opt) != ($argc - 1)){
+  fwrite(STDERR, "10: Invalid parameters! Try --help.\n");
+  exit(10);
+}
+
+if(isset($opt["help"])){
+  if($argc > 2){
+    fwrite(STDERR, "10: Invalid parameters! Try --help.\n");
+    exit(10);
+  } else {
+  fwrite(STDOUT, "This is some help.\n");
+  exit(0);
+  }
+}
+
+if((isset($opt["comments"]) || isset($opt["loc"])) && !isset($opt["stats"])){
+  fwrite(STDERR, "10: Invalid parameters! Try --help.\n");
+  exit(10);
+}
+
 function getArg($arg){
   $type = "";
   $output = "";
@@ -62,19 +84,6 @@ function getArg($arg){
   return array($type, $output);
 }
 
-if($argc == 2){
-  if($argv[1] == "--help"){
-    fwrite(STDOUT, "This is some help.\n");
-    exit(0);
-  } else {
-    fwrite(STDERR, "10: Unrecognized argument! Try --help.\n");
-    exit(10);
-  }
-} else if($argc > 2){
-  fwrite(STDERR, "10: Wrong number of arguments!\n");
-  exit(10);
-}
-
 if(strtolower(trim(fgets(STDIN))) != ".ippcode18"){
   fwrite(STDERR, "21: .IPPcode18 header missing!\n");
   exit(21);
@@ -91,11 +100,15 @@ xmlwriter_text($xml, 'IPPcode18');
 xmlwriter_end_attribute($xml);
 
 $order = 0;
+$comments = 0;
 
 while(!feof(STDIN)){
   $line = trim(preg_replace('/\s+/', ' ', fgets(STDIN)));
   $comment = strpos($line, "#");
-  if($comment !== false) $line = rtrim(substr($line, 0, $comment));
+  if($comment !== false){
+    $line = rtrim(substr($line, 0, $comment));
+    $comments++;
+  }
   if($line == "") continue;
   $order++;
   $word = explode(' ', $line);
@@ -113,24 +126,12 @@ while(!feof(STDIN)){
   xmlwriter_text($xml, $word[0]);
   xmlwriter_end_attribute($xml);
 
-/*
-  xmlwriter_start_attribute($xml, '');
-  xmlwriter_text($xml, '');
-  xmlwriter_end_attribute($xml);
-*/
-
-  //echo "\"".$line."\"\n";
-  //for($i=0; $i<$count; $i++) echo $word[$i]."\n";
-
-  if($count > 4){
-    fwrite(STDERR, "21: Too many operands of instruction '$word[0]'!\n");
-    exit(21);
-  }
-
   // no operands
-  if($count == 1 && !in_array($word[0], array("CREATEFRAME", "PUSHFRAME", "POPFRAME", "RETURN", "BREAK"))){
-    fwrite(STDERR, "21: Unknown instruction '$word[0]'!\n");
-    exit(21);
+  if($count == 1){
+    if(!in_array($word[0], array("CREATEFRAME", "PUSHFRAME", "POPFRAME", "RETURN", "BREAK"))){
+      fwrite(STDERR, "21: Unknown instruction '$word[0]'!\n");
+      exit(21);
+    }
   }
 
   // single operand
@@ -158,49 +159,75 @@ while(!feof(STDIN)){
 
   // 2 operands
   else if($count == 3){
+    $arg1 = getArg($word[1]);
+    $arg2 = getArg($word[2]);
     // <var> <symb>
-    if(!in_array($word[0], array("MOVE", "INT2CHAR", "STRLEN", "TYPE"))){
+    if((!in_array($word[0], array("MOVE", "INT2CHAR", "STRLEN", "TYPE", "NOT")) || $arg1[0] != "var" || !in_array($arg2[0], array("var", "int", "bool", "string"))) &&
+    // <var> <type>
+    ($word[0] != "READ" || $arg1[0] != "var" || $arg2[0] != "type")
+    ){
       fwrite(STDERR, "21: Unknown instruction '$word[0]'!\n");
       exit(21);
+    } else {
+      xmlwriter_start_element($xml, 'arg1');
+      xmlwriter_start_attribute($xml, 'type');
+      xmlwriter_text($xml, $arg1[0]);
+      xmlwriter_end_attribute($xml);
+      xmlwriter_text($xml, $arg1[1]);
+      xmlwriter_end_element($xml);
+
+      xmlwriter_start_element($xml, 'arg2');
+      xmlwriter_start_attribute($xml, 'type');
+      xmlwriter_text($xml, $arg2[0]);
+      xmlwriter_end_attribute($xml);
+      xmlwriter_text($xml, $arg2[1]);
+      xmlwriter_end_element($xml);
     }
   }
 
   // 3 operands
   else if($count == 4){
-
-    // 6.4.3
-    if($word[0] == "ADD");
-    else if($word[0] == "SUB");
-    else if($word[0] == "MUL");
-    else if($word[0] == "IDIV");
-
-    else if($word[0] == "LT");
-    else if($word[0] == "GT");
-    else if($word[0] == "EQ");
-
-    else if($word[0] == "AND");
-    else if($word[0] == "OR");
-    else if($word[0] == "NOT");
-
-    else if($word[0] == "STRI2INT");
-
-    // 6.4.5
-    else if($word[0] == "CONCAT");
-    else if($word[0] == "GETCHAR");
-    else if($word[0] == "SETCHAR");
-
-    // 6.4.7
-    else if($word[0] == "JUMPIFEQ");
-    else if($word[0] == "JUMPIFNEQ");
-
-    else {
-      fwrite(STDERR, "21: Unknown instruction '$word[0]'!\n");
+    $arg1 = getArg($word[1]);
+    $arg2 = getArg($word[2]);
+    $arg3 = getArg($word[3]);
+    if(!in_array($arg2[0], array("var", "int", "bool", "string")) || !in_array($arg3[0], array("var", "int", "bool", "string"))){
+      fwrite(STDERR, "21: Invalid instruction '$word[0]'!\n");
       exit(21);
     }
+    // <var> <symb> <symb>
+    if((!in_array($word[0], array("ADD", "SUB", "MUL", "IDIV", "LT", "GT", "EQ", "AND", "OR", "STRI2INT", "CONCAT", "GETCHAR", "SETCHAR")) || $arg1[0] != "var") &&
+    // <label> <symb> <symb>
+    (!in_array($word[0], array("JUMPIFEQ", "JUMPIFNEQ")) || $arg1[0] != "label")
+    ){
+      fwrite(STDERR, "21: Invalid instruction '$word[0]'!\n");
+      exit(21);
+    } else {
+      xmlwriter_start_element($xml, 'arg1');
+      xmlwriter_start_attribute($xml, 'type');
+      xmlwriter_text($xml, $arg1[0]);
+      xmlwriter_end_attribute($xml);
+      xmlwriter_text($xml, $arg1[1]);
+      xmlwriter_end_element($xml);
 
+      xmlwriter_start_element($xml, 'arg2');
+      xmlwriter_start_attribute($xml, 'type');
+      xmlwriter_text($xml, $arg2[0]);
+      xmlwriter_end_attribute($xml);
+      xmlwriter_text($xml, $arg2[1]);
+      xmlwriter_end_element($xml);
+
+      xmlwriter_start_element($xml, 'arg3');
+      xmlwriter_start_attribute($xml, 'type');
+      xmlwriter_text($xml, $arg3[0]);
+      xmlwriter_end_attribute($xml);
+      xmlwriter_text($xml, $arg3[1]);
+      xmlwriter_end_element($xml);
+    }
+
+  } else {
+    fwrite(STDERR, "21: Too many operands of instruction '$word[0]'!\n");
+    exit(21);
   }
-
-  else if($count == 3);
 
   xmlwriter_end_element($xml);
 }
@@ -208,7 +235,26 @@ while(!feof(STDIN)){
 xmlwriter_end_element($xml);
 xmlwriter_end_document($xml);
 
-echo xmlwriter_output_memory($xml);
+// xml output
+//echo xmlwriter_output_memory($xml);
+
+// stats output
+if(isset($opt["stats"])){
+  $file = fopen($opt["stats"], "w");
+  // loc only
+  if(isset($opt["loc"]) && !isset($opt["comments"])) fwrite($file, $order."\n");
+  // comments only
+  else if(isset($opt["comments"]) && !isset($opt["loc"])) fwrite($file, $comments."\n");
+  // both loc and comments
+  else if(isset($opt["loc"]) && isset($opt["comments"])){
+    if(array_search("--loc", $argv) < array_search("--comments", $argv)) $stats = array($order, $comments);
+    else $stats = array($comments, $order);
+    fwrite($file, $stats[0]."\n");
+    fwrite($file, $stats[1]."\n");
+  }
+  fclose($file);
+}
+
 exit(0);
 
 ?>
