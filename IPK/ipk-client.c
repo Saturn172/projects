f47dadf6f736b9a182eaba697d821d29a4620277
Saturn172 <47@ica.org>
2018-03-08 01:22:26 +0100

    IPK - socket test

--- IPK/ipk-client.c
+++ IPK/ipk-client.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 
 int main(){
-  printf("Taky nedělám nic.\n");
+  // creating socketa
+  if ((client_socket = socket(AF_INET, SOCK_DGRAM, 0)) <= 0){
+    perror("ERROR in socket");
+    exit(EXIT_FAILURE);
+  }
   return 0;
 }
