1dc1b2ac562b14524ad6168dc7e434d0cc38ef56
Saturn172 <47@ica.org>
2018-03-08 12:09:34 +0100

    IPK socket test 2

--- IPK/ipk-client.c
+++ IPK/ipk-client.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+//#include <netinet/in.h>
+#include <netdb.h>
 
 int main(){
-  // creating socketa
+  int client_socket;
+  // creating socket
   if ((client_socket = socket(AF_INET, SOCK_DGRAM, 0)) <= 0){
-    perror("ERROR in socket");
-    exit(EXIT_FAILURE);
+    fprintf(stderr, "ERROR in socket");
+    return EXIT_FAILURE;
   }
+  printf("Socket number: %d\n", client_socket);
   return 0;
 }
