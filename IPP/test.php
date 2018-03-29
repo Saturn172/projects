e6767e8c735f94eb9a570d37ccd47c561f4bd333
Saturn172 <47@ica.org>
2018-03-29 14:26:46 +0200

    IPP - interpret.py v1.3

--- IPP/test.php
+++ IPP/test.php
@@ -1,129 +1,138 @@
-<html>
+#!/usr/bin/php5.6
+<?php
 
-  <head>
-  <meta http-equiv="content-type" content="text/html; charset=utf-8">
-  <title>IPP project - Results</title>
-  </head>
+$help = "
+Loads tests to check proper function of parse.php and interpret.py.
 
-  <body style="color: white; background-color: black; font-family: arial; text-align: center">
-    <h1 style="font-family: impact">IPP PROJECT</h1>
-    <h2 style="font-family: impact">test.php</h2>
+Usage: ./test.php [--help] [--directory=dir] [--recursive] [--parse-script=script] [--int-script=script]
 
-    <?php
-
-    $help = "
-Some helping shit.
+--help                  shows this help (cannot combine with any other parameter)
+--directory=path        specifies directory where script will search for tests
+--recursive             script will search also in subdirectories
+--parse-script=path     specifies path to parse script (default: ./parse.php)
+--int-script=path       specifies path to interpret script (default: ./interpret.py)
 ";
 
-    $opt = getopt("", array("help", "directory:", "recursive", "parse-script:", "int-script:"));
+$opt = getopt("", array("help", "directory:", "recursive", "parse-script:", "int-script:"));
 
-    if(count($opt) != ($argc - 1)){
-      fwrite(STDERR, "10: Invalid parameters! Try --help.\n");
-      exit(10);
+if(count($opt) != ($argc - 1)){
+  fwrite(STDERR, "10: Invalid parameters! Try --help.\n");
+  exit(10);
+}
+
+if(isset($opt["help"])){
+  if($argc > 2){
+    fwrite(STDERR, "10: Invalid parameters! Try --help.\n");
+    exit(10);
+  } else {
+    fwrite(STDOUT, $help."\n");
+    exit(0);
+  }
+}
+
+if(isset($opt["directory"])){
+  $path = $opt["directory"];
+} else {
+  $path = ".";
+}
+
+if(isset($opt["recursive"])){
+  $rec = "";
+} else {
+  $rec = "-maxdepth 1";
+}
+
+if(isset($opt["parse-script"])){
+  $parse = $opt["parse-script"];
+} else {
+  $parse = "./parse.php";
+}
+
+if(isset($opt["int-script"])){
+  $int = $opt["int-script"];
+} else {
+  $int = "./interpret.py";
+}
+
+$test = explode("\n", shell_exec("find $path/ -name '*.src' $rec 2>/dev/null"));
+$N = count($test) - 1;
+
+$errors = 0;
+$failed = [];
+
+for($i = 0; $i < $N; $i++){
+  // file for parse.php output
+  $temp1 = tmpfile();
+  $MD = stream_get_meta_data($temp1);
+  $source = $MD['uri'];
+
+  // file for interpret.py output
+  $temp2 = tmpfile();
+  $MD = stream_get_meta_data($temp2);
+  $stdout = $MD['uri'];
+
+  // interpret.py input
+  if(($in = @fopen(substr($test[$i], 0, -3)."in", "r")) === false) $stdin = "";
+  else {
+    $stdin = fread($in, filesize(substr($test[$i], 0, -3)."in"));
+    fclose($in);
+  }
+
+  // interpret.py sample output
+  if(file_exists(substr($test[$i], 0, -3)."out") === false){
+    $tmp3 = tmpfile();
+    $MD = stream_get_meta_data($temp2);
+    $sample = $MD['uri'];
+  } else {
+    $sample = substr($test[$i], 0, -3)."out";
+  }
+
+  // proper return value
+  if(($rc = @fopen(substr($test[$i], 0, -3)."rc", "r")) === false) $retval = 0;
+  else {
+    $retval = fgets($rc);
+    fclose($rc);
+  }
+
+  // execute parse.php, store XML
+  exec("cat '$test[$i]' | php5.6 '$parse' 2>/dev/null", $shell, $retcode);
+  fwrite($temp1, implode("\n", $shell)."\n");
+
+  // clear shell output
+  $shell = [];
+
+  // check retcode after parse.php
+  if($retcode == 0){
+
+    // execute interpret.py, store output
+    exec("echo '$stdin' | python3.6 '$int' --source='$source' 2>/dev/null", $shell, $retcode);
+    fwrite($temp2, implode("\n", $shell)."\n");
+
+    // diff
+    if($retcode != $retval || shell_exec("diff '$stdout' '$sample'") != ""){
+      $errors++;
+      $failed[count($failed)] = $test[$i];
     }
 
-    if(isset($opt["help"])){
-      if($argc > 2){
-        fwrite(STDERR, "10: Invalid parameters! Try --help.\n");
-        exit(10);
-      } else {
-        fwrite(STDOUT, $help."\n");
-        exit(0);
-      }
+  // parse.php havent returned anything
+  } else {
+    if($retcode != $retval){
+      $errors++;
+      $failed[count($failed)] = substr($test[$i], 0, -4);
     }
+  }
+}
 
-    if(isset($opt["directory"])){
-      $path = $opt["directory"];
-    } else {
-      $path = ".";
-    }
+echo "<html>";
 
-    if(isset($opt["recursive"])){
-      $rec = "";
-    } else {
-      $rec = "-maxdepth 1";
-    }
+  echo "<head>";
+    echo "<meta http-equiv='content-type' content='text/html; charset=utf-8'>";
+    echo "<title>IPP project - Results</title>";
+  echo "</head>";
 
-    if(isset($opt["parse-script"])){
-      $parse = $opt["parse-script"];
-    } else {
-      $parse = "./parse.php";
-    }
-
-    if(isset($opt["int-script"])){
-      $int = $opt["int-script"];
-    } else {
-      $int = "./interpret.py";
-    }
-
-    $test = explode("\n", shell_exec("find $path/ -name '*.src' $rec 2>/dev/null"));
-    $N = count($test) - 1;
-
-    $errors = 0;
-    $failed = [];
-
-    for($i = 0; $i < $N; $i++){
-      // file for parse.php output
-      $temp1 = tmpfile();
-      $MD = stream_get_meta_data($temp1);
-      $source = $MD['uri'];
-
-      // file for interpret.py output
-      $temp2 = tmpfile();
-      $MD = stream_get_meta_data($temp2);
-      $stdout = $MD['uri'];
-
-      // interpret.py input
-      if(($in = @fopen(substr($test[$i], 0, -3)."in", "r")) === false) $stdin = "";
-      else {
-        $stdin = fread($in, filesize(substr($test[$i], 0, -3)."in"));
-        fclose($in);
-      }
-
-      // interpret.py sample output
-      if(file_exists(substr($test[$i], 0, -3)."out") === false){
-        $tmp3 = tmpfile();
-        $MD = stream_get_meta_data($temp2);
-        $sample = $MD['uri'];
-      } else {
-        $sample = substr($test[$i], 0, -3)."out";
-      }
-
-      // proper return value
-      if(($rc = @fopen(substr($test[$i], 0, -3)."rc", "r")) === false) $retval = 0;
-      else {
-        $retval = fgets($rc);
-        fclose($rc);
-      }
-
-      // execute parse.php, store XML
-      exec("cat '$test[$i]' | php5.6 '$parse' 2>/dev/null", $shell, $retcode);
-      fwrite($temp1, implode("\n", $shell)."\n");
-
-      // clear shell output
-      $shell = [];
-
-      // check retcode after parse.php
-      if($retcode == 0){
-
-        // execute interpret.py, store output
-        exec("echo '$stdin' | python3.6 '$int' --source='$source' 2>/dev/null", $shell, $retcode);
-        fwrite($temp2, implode("\n", $shell)."\n");
-
-        // diff
-        if($retcode != $retval || shell_exec("diff '$stdout' '$sample'") != ""){
-          $errors++;
-          $failed[count($failed)] = $test[$i];
-        }
-
-      // parse.php havent returned anything
-      } else {
-        if($retcode != $retval){
-          $errors++;
-          $failed[count($failed)] = substr($test[$i], 0, -4);
-        }
-      }
-    }
+  echo "<body style='color: white; background-color: black; font-family: arial; text-align: center'>";
+    echo "<h1 style='font-family: impact'>IPP PROJECT</h1>";
+    echo "<h2 style='font-family: impact'>test.php</h2>";
 
     if($N == 0) echo "<span style='color: red'><b>No tests found!</b></span>";
     else {
@@ -143,8 +152,7 @@ Some helping shit.
       }
     }
 
-    ?>
+  echo "</body>";
+echo "</html>";
 
-  </body>
-
-</html>
+?>
